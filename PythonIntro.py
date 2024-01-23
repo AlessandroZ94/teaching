751849197b5d5cff7b250c090241accd95eba2fb
Alessandro Zecchi <alessandro.zecchi94@gmail.com>
2024-01-23 08:27:54 +0100

    Intro

---
+++ PythonIntro.py
@@ -0,0 +1,49 @@
+# This is a comment. It starts with a # symbol and is ignored by the computer.
+# Comments are useful for explaining what the code does and why.
+
+# A variable is a name that refers to a value. You can assign a value to a variable using the = symbol.
+# For example, this assigns the value 10 to the variable x:
+x = 10
+
+# You can print the value of a variable using the print function. The print function displays the value inside the parentheses on the screen.
+# For example, this prints the value of x:
+print(x)
+
+# You can also print a string, which is a sequence of characters enclosed in quotes. A string can be anything you want, such as your name, a message, or a word.
+# For example, this prints the string "Hello, world!":
+print("Hello, world!")
+
+# You can use arithmetic operators to perform calculations with variables and values. The basic operators are + for addition, - for subtraction, * for multiplication, / for division, and ** for exponentiation.
+# For example, this assigns the value of x + 5 to the variable y:
+y = x + 5
+
+# You can print the result of an arithmetic operation using the print function as well.
+# For example, this prints the value of y:
+print(y)
+
+# You can use comparison operators to compare two values or variables. The basic operators are == for equality, != for inequality, < for less than, > for greater than, <= for less than or equal to, and >= for greater than or equal to.
+# The result of a comparison is either True or False, which are called boolean values.
+# For example, this compares the values of x and y and assigns the result to the variable z:
+z = x == y
+
+# You can print the result of a comparison using the print function as well.
+# For example, this prints the value of z:
+print(z)
+
+# You can use conditional statements to execute different blocks of code depending on some condition. The basic syntax is if condition: followed by an indented block of code. You can also use else: followed by another indented block of code to execute when the condition is False.
+# For example, this prints a message depending on whether x is equal to y or not:
+if x == y:
+    print("x and y are equal")
+else:
+    print("x and y are not equal")
+
+# You can also use elif: followed by another condition and an indented block of code to check for multiple conditions in order.
+# For example, this prints a message depending on whether x is positive, negative, or zero:
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is zero")
+
+# This is the end of the code. You can run it in any python interpreter or online editor and see what it does.
